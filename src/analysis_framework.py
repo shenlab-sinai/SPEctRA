6d151eb1a481c5c17fd3808b93be4e1fd704f54e
Immanuel Purushothaman <ipurusho@asu.edu>
2013-12-22 15:56:01 -0500

    returns fastq strings in comma-separated form for R1 and R2

--- src/analysis_framework.py
+++ src/analysis_framework.py
@@ -4,12 +4,14 @@ import os
 import glob
 import sys
 import re
+from utils import *
 
 #Classes/methods to get data and prepare it for specified analysis tasks
 
 class GatherData(object):
 	#sort fastq samples to R1 and R2, even if multiple fastqs for one end are available
-	def studySamples(self, fastq):
+	util = Utility()
+	def studySamples(self, fastq): #need to sort values 
 	
 		exp = "R[1|2]"
 		samples = { }
@@ -20,10 +22,24 @@ class GatherData(object):
 				samples.update({line:read[0]})
 		#return dictionary of values		
 		return samples		
+	#these functions should parse R1 and R2 data and return a string 
+	def read1(self,fastq):
+		R1 = ",".join([str(x) for x in self.util.findKey(self.studySamples(fastq),'R1')])
+		return R1
 
- 
+	def read2(self,fastq):
+		R2 = ",".join([str(x) for x in self.util.findKey(self.studySamples(fastq),'R2')])
+		return R2
 
 word = ["I_100_bc20_GTGGCC_L001_R1_001.C3H70ACXX.fastq.gz","I_100_bc20_GTGGCC_L001_R2_001.C3H70ACXX.fastq.gz","I_100_bc20_GTGGCC_L002_R1_001.C3H70ACXX.fastq.gz","I_100_bc20_GTGGCC_L002_R2_001.C3H70ACXX.fastq.gz"]
 
 test = GatherData()
+#test.studySamples['R1']
 
+# list comprehension is the best print ",".join([str(x) for x in word])  
+print test.read1(word)
+
+
+
+
+#mydict.keys()[mydict.values().index(16)]
--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,11 @@
 import os
 import glob
 import sys
+import re
 
 #python class to manage auxilary file/string manipulation
 class Utility(object):
-	#iterates through a directory and lists subdirectories
+	#iterates through a directory and lists subdirectories, use with glob method too
 	def subDirectories(self,dir):
 		return [name for name in os.listdir(dir)
            if os.path.isdir(os.path.join(dir, name))]
@@ -14,5 +15,5 @@ class Utility(object):
 		return openFile
 	def mappedBam(self): #get generic mapped reads filename (different from STAR and Tophat2), may move outside of utils class
 		pass
-
-		
+	def findKey(self,input_dict, value): #dict search method
+		return {k for k, v in input_dict.items() if v == value}
