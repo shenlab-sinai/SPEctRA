783923f7043c154b2c4252b8cf8598ead659c4e7
Immanuel Purushothaman <ipurusho@asu.edu>
2013-12-28 21:01:17 -0500

    settings class created, use yaml libraries, related to #6

--- config/env_config.py
+++ config/env_config.py
@@ -4,4 +4,19 @@ import sys
 import yaml
 
 class ImportSettings(object):
-	pass
+	def __init__(self, config):
+		self.config = config
+	def openConfig(self):
+		with open('config.yaml', 'r') as f:
+			doc = yaml.load(f)
+		return doc
+	def getEnv(self):
+		pass
+	def homeDir(self):
+		pass 
+	def genomes(self):
+		pass
+		#return dict of genomes and their paths
+	def paths(self):
+		#return dict of available short-read aligner paths
+		pass
