b03028c6934222261fae050ca74dfd46146d820d
Immanuel Purushothaman <ipurusho@asu.edu>
2013-12-31 16:54:36 -0500

    auto qusub method added

--- src/utils.py
+++ src/utils.py
@@ -16,3 +16,6 @@ class Utility(object):
 		return openFile
 	def findKey(self,input_dict, value): #dict search method, find key based on value, returns  all in list
 		return {k for k, v in input_dict.items() if v == value}
+	def batchQsub(self,directory): #launch qsub scripts into the respective queue
+		for line in glob.glob(directory +"/*.pbs"):
+			os.system("qsub " + line)
