1b71e29cf2121fb9f2586bdd00d0e79337a82ea0
Immanuel Purushothaman <ipurusho@asu.edu>
2014-01-02 12:20:38 -0500

    qsub capability example

--- src/complete_seq.py
+++ src/complete_seq.py
@@ -2,13 +2,15 @@
 import optparse
 from analysis_framework import *
 from env_config import *
+from utils import *
+
 
 parser = optparse.OptionParser()
 parser.add_option('-p', dest="pipeline_input")
 options, remainder = parser.parse_args()
 
 settings = ImportSettings() #enviroment configuration settings
-
+util = Utility()
 def main():
 	
 
@@ -19,6 +21,7 @@ def main():
 	directories.makeProj() #project directory created
 	directories.startMappingEnv() #mapping subdirectories created
 	writer.writeMappingScript(options.pipeline_input)
+	util.batchQsub(settings.homeDir()+"/"+pipeline_command.projName()+"/scripts/mapping/")
 
 if __name__ == '__main__':
     main()
