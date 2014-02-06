332118e7e4dd775fb7476b4f81711ba0e4487b44
Immanuel Purushothaman <ipurusho@asu.edu>
2014-02-05 20:48:01 -0500

    replicates from two separate sequencing runs can be mapped in one go

--- src/complete_seq.py
+++ src/complete_seq.py
@@ -7,6 +7,7 @@ from utils import *
 
 parser = optparse.OptionParser()
 parser.add_option('-p', dest="pipeline_input")
+parser.add_option('-m', dest="merge_table")
 options, remainder = parser.parse_args()
 
 settings = ImportSettings() #enviroment configuration settings
@@ -22,8 +23,11 @@ def main():
 		
 		directories.makeProj() #project directory created
 		directories.startMappingEnv() #mapping subdirectories created
-		writer.writeMappingScript(options.pipeline_input)
-		util.batchQsub(settings.homeDir()+"/"+pipeline_command.projName()+"/scripts/mapping/")
+		writer.writeMappingScript(options.pipeline_input,options.merge_table)
+
+		#if pipeline_command.openConfig()["mapping"][ merge_directory_path] is not None:
+			#mergetable outer logic
+		#util.batchQsub(settings.homeDir()+"/"+pipeline_command.projName()+"/scripts/mapping/") #omit for testing purposes
 
 if __name__ == '__main__':
     main()
