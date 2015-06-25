a2074c94fc6faa1e9ea1a1ad9626c7d48ea824ba
Immanuel Purushothaman <ipurusho@asu.edu>
2015-06-25 10:14:37 -0400

    code cleanup using sublime PEP8 autoformatting and added batchExecute function

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python
 import os
 import glob
-import sys
-import re
-import ConfigParser
 
-#python class to manage auxilary file/string manipulation
+
+# python class to manage auxilary file/string manipulation
 class Utility(object):
-	#iterates through a directory and lists subdirectories, use with glob method too
-	def subDirectories(self,dir):
-		return [name for name in os.listdir(dir)
-           if os.path.isdir(os.path.join(dir, name))]
-	def openFile(self,dir,fileName): #opens file, read only
-		openFile = open(dir+"/"+fileName,"rb")
-		return openFile
-	def findKey(self,input_dict, value): #dict search method, find key based on value, returns  all in list
-		return {k for k, v in input_dict.items() if v == value}
-	def batchQsub(self,directory): #launch qsub scripts into the respective queue
-		for line in glob.glob(directory +"/*.pbs"):
-			os.system("qsub " + line)
-	def batchBsub(self,directory): #launch qsub scripts into the respective queue
-		for line in glob.glob(directory +"/*.lsf"):
-			os.system("bsub <" + line)
+    # iterates through a directory and lists subdirectories, use with glob
+    # method too
+
+    def subDirectories(self, dir):
+        return [name for name in os.listdir(dir)
+                if os.path.isdir(os.path.join(dir, name))]
+
+    def openFile(self, dir, fileName):  # opens file, read only
+        openFile = open(dir + "/" + fileName, "rb")
+        return openFile
+
+    # dict search method, find key based on value, returns  all in list
+    def findKey(self, input_dict, value):
+        return {k for k, v in input_dict.items() if v == value}
+
+    # launch qsub scripts into the respective queue
+    def batchQsub(self, directory):
+        for line in glob.glob(directory + "/*.pbs"):
+            os.system("qsub " + line)
+
+    # launch qsub scripts into the respective queue
+    def batchBsub(self, directory):
+        for line in glob.glob(directory + "/*.lsf"):
+            os.system("bsub <" + line)
+
+    def batchExecute(self, directory):
+        for line in glob.glob(directory + "/*.sh"):
+            os.system("./" + line)
